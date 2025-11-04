57d739120b8f56c8485d2a7768107c7d71b88256
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2025-11-04 21:20:18 +0900

    Add files via upload

---
+++ リモコン/switch.py
@@ -0,0 +1,23 @@
+import socket
+import RPi.GPIO as GPIO
+
+RIGHT = 5
+LEFT = 3
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LEFT, GPIO.IN)
+GPIO.setup(RIGHT, GPIO.IN)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    msg = ""
+    if GPIO.input(LEFT):
+        msg = "1"
+    else:
+        msg = "0"
+    msg = msg + ","
+    if GPIO.input(RIGHT):
+        msg = msg + "1"
+    else:
+        msg = msg + "0"
+    sock.sendto(msg.encode("utf-8"), ("10.153.229.254", 9999))
